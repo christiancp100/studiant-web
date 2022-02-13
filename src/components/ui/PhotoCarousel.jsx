f0ad9626d5539121ac43826084c87bc3a7d5dd09
Lucia <luciacastrobujan@gmail.com>
2022-02-13 21:32:56 +0100

    Create the apartment images carousel

---
+++ src/components/ui/PhotoCarousel.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import { Carousel } from 'react-responsive-carousel';
+
+const PhotoCarousel = () => {
+  const img = '/img/apartment.jpeg';
+  const img2 = '/img/building.jpeg';
+  const img3 = '/img/house.svg';
+  const img4 = '/img/apartment.jpeg';
+  const img5 = '/img/building.jpeg';
+  const img6 = '/img/house.svg';
+  const imgs = [img, img2, img3, img4, img5, img6];
+
+  return (
+    <Carousel showStatus={false} onClickItem={() => {}}>
+      {imgs &&
+        imgs.map((img, i) => (
+          <div key={i}>
+            <img className="object-cover" src={img} />
+          </div>
+        ))}
+    </Carousel>
+  );
+};
+
+PhotoCarousel.displayName = 'PhotoCarousel';
+
+export default PhotoCarousel;

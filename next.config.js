5e62faf2d15ef1b8175a3de7851bc2703516582f
Christian Capeans <christiancp100@gmail.com>
2022-07-23 19:30:43 +0200

    Change images url

--- next.config.js
+++ next.config.js
@@ -4,7 +4,6 @@ module.exports = {
     backendUrl: process.env.API_URL || 'http://localhost:1337',
   },
   images: {
-    deviceSizes: [320, 420, 768, 1024, 1200],
     loader: 'default',
     domains: [process.env.IMAGES_URL || 'localhost'],
   },
